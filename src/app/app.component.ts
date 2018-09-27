49cdc4e421ffa13277d7aec6b6f3d79a2e8482d2
Robbie Martinus <12573669+rmartinus@users.noreply.github.com>
2018-09-27 21:00:57 +1000

    Upload file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'image-analyser-angular-gcp';
+  selectedFile: File;
+  http: HttpClient;
+
+  constructor(private http: HttpClient) {
+    this.http = http
+  }
+
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0]
+  }
+
+  onUpload() {
+    const uploadData = new FormData();
+    uploadData.append('image', this.selectedFile, this.selectedFile.name);
+
+    this.http.post('https://my-image-analyser.appspot.com/v1/analyse', uploadData,  {
+      reportProgress: true,
+      observe: 'events'
+    })
+    .subscribe(event => {
+      console.log(event); // handle event here
+    });
+  }
 }
